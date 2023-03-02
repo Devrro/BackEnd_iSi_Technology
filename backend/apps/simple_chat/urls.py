246e6452f75fa1e129839119dbad9fd7fe9d2711
Devrro <serhii.terletskiy.ua@gmail.com>
2023-03-02 02:15:56 +0200

    Rename variables

--- backend/apps/simple_chat/urls.py
+++ backend/apps/simple_chat/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path("/create_thread", ThreadListCreateView.as_view(), name="create_thread_view"),
     path("/delete_thread/<int:thread_id>", DeleteThreadView.as_view(), name="create_thread_view"),
     path("/list_thread_by_user_id/<int:user_id>", ThreadListByUserIdView.as_view(), name="list_threads_by_user"),
-    path("/create_message_in_thread/<int:thread>/<int:sender>", MessageCreateView.as_view(),
+    path("/create_message_in_thread/<int:thread_id>/<int:sender_id>", MessageCreateView.as_view(),
          name="create_message_view"),
     path("/get_messages_in_thread/<int:thread_id>", MessageInThreadListView.as_view(), name="get_messages_in_thread"),
     path("/is_messages_read/", MessagesIsRead.as_view(), name="is_messages_read"),
